0a3b6358c5ba70e1723f510925e43dc22e5279a1
Joy Victor <69626754+Jhoie@users.noreply.github.com>
2020-08-13 13:02:42 +0100

    Add files via upload

---
+++ Recursion Assignment.py
@@ -0,0 +1,20 @@
+def fib(num):
+    if num <= 1:
+        return num
+    else:
+        return fib(num - 1) + fib(num - 2)
+
+
+try:
+    num_terms = int(input("Enter any number: "))
+
+    if num_terms <= 0:
+        print("Please enter any digit greater than 0")
+    else:
+        print("Fibonacci sequence:")
+        for i in range(num_terms):
+            result = fib(i)
+            print(result)
+
+except ValueError:
+    print("Enter any digit greater than 0")
